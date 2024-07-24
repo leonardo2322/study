9c700d0fbae02434ca65909b7b6d31af0dea753c
unknown <joys23services@gmail.com>
2024-07-24 14:48:15 -0400

    colors hexadecimal

---
+++ colors.py
@@ -0,0 +1,10 @@
+greenColour = "\x1b[3;32m"#formato hexadecimal
+yellowColour = '\033[1;33m'#formato octal
+redColor= "\033[1;31m"
+endColour = "\033[0m"#finaliza color
+whiteColour = "\033[1;37m"
+cianColour = "\033[1;36m"
+blackColour = "\033[1;30m"
+blueColour = "\033[1;34m"
+purpleColour = "\033[1;35m"
+fondoblanco = "\033[1;32;47m"
